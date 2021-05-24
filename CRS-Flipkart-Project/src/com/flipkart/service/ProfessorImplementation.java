4fff17db741d5d8376e7619f8ca0a5d3989e5c09
Hrithvik Porwal <h.hrithvikporwal@gmail.com>
2021-05-24 20:00:47 +0530

    updated professor interface and its implementation

--- CRS-Flipkart-Project/src/com/flipkart/service/ProfessorImplementation.java
+++ CRS-Flipkart-Project/src/com/flipkart/service/ProfessorImplementation.java
@@ -3,10 +3,35 @@
  */
 package com.flipkart.service;
 
+import java.util.List;
+
+import com.flipkart.bean.Course;
+import com.flipkart.bean.Student;
+
 /**
  * @author hp
  *
  */
-public class ProfessorImplementation {
+public class ProfessorImplementation implements ProfessorInterface {
+
+	@Override
+	public List<Course> getCourses(String professorId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Student> viewStudents(String professorId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addGrade(String studentId, String courseId, String grade) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
 
 }
