f0cfb754b9e6c84ba820fb9696c356655d584b98
shubham sharma <shubhamsharma000231@gmail.com>
2021-05-24 19:08:06 +0530

    Course and Notification Bean classes

---
+++ CRS-Flipkart-Project/src/com/flipkart/bean/Notification.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.flipkart.bean;
+
+/**
+ * @author lenovo
+ *
+ */
+public class Notification {
+	
+	private String notificationId;
+	private String message;
+	private String paymentId;
+	private String studentId;
+	
+	
+	public String getNotificationId() {
+		return notificationId;
+	}
+	public void setNotificationId(String notificationId) {
+		this.notificationId = notificationId;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public String getPaymentId() {
+		return paymentId;
+	}
+	public void setPaymentId(String paymentId) {
+		this.paymentId = paymentId;
+	}
+	
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	
+	
+
+}
