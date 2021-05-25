8037deae80273513d5937e5bd7ea0751619ac422
namanmanish <namanmanishtrivedi@gmail.com>
2021-05-25 13:39:01 +0530

    Addeed exceptions in student

--- CRS-Flipkart-Project/src/com/flipkart/service/StudentOperation.java
+++ CRS-Flipkart-Project/src/com/flipkart/service/StudentOperation.java
@@ -2,24 +2,26 @@ package com.flipkart.service;
 
 import com.flipkart.bean.ReportCard;
 import com.flipkart.bean.Student;
+import com.flipkart.exception.StudentNotRegisteredException;
+import com.flipkart.exception.UserAlreadyExistException;
 
 public class StudentOperation implements StudentInterface {
 
 	@Override
-	public Boolean checkIsVerified(Student student) {
+	public Boolean checkIsVerified(Student student) throws StudentNotRegisteredException  {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public ReportCard viewReportCard(Student student) {
+	public ReportCard viewReportCard(Student student) throws StudentNotRegisteredException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Boolean registerStudent(String userName, String phoneNumber, String address, String userId,
-			String userPassword, String type, String rollNumber, String branch, Boolean isVerified) {
+			String userPassword, String type, String rollNumber, String branch, Boolean isVerified) throws UserAlreadyExistException{
 		// TODO Auto-generated method stub
 		return null;
 	}
