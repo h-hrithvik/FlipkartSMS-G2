7acb65d776477bfbaf867484b52d3642caa6e5dd
Anshuman Kumar <anshinanu@yahoo.com>
2021-05-25 13:57:54 +0530

    added comments to interface

--- CRS-Flipkart-Project/src/com/flipkart/service/RegistrationInterface.java
+++ CRS-Flipkart-Project/src/com/flipkart/service/RegistrationInterface.java
@@ -8,16 +8,52 @@ import com.flipkart.exception.CourseNotDeletedException;
 import com.flipkart.exception.PaymentNotFoundException;
 
 public interface RegistrationInterface {
+	/**
+	 *
+	 * @param courseId
+	 * @param studentId
+	 * @param courseList
+	 * @return true/false for successful/unsuccessful operation of adding a course.
+	 * @throws AddCourseException
+	 */
 	public boolean addCourse(String courseId, String studentId, List<Course> courseList) throws AddCourseException;
-	
+
+	/**
+	 *
+	 * @param courseId
+	 * @param studentId
+	 * @param registeredCourseList
+	 * @return true/false for successful/unsuccessful operation of dropping a course.
+	 * @throws CourseNotDeletedException
+	 */
 	public boolean dropCourse(String courseId, String studentId, List<Course> registeredCourseList) throws CourseNotDeletedException;
-	
+
+	/**
+	 *
+	 * @return list of courses available
+	 */
 	public List<Course> viewCourses();
-	
+
+	/**
+	 *
+	 * @param studentId
+	 * @return list of registered courses by a student.
+	 */
 	public List<Course> viewRegisteredCourses(String studentId);
-	
+
+	/**
+	 *
+	 * @param studentId
+	 * @return report card of the student as an object of class ReportCard
+	 */
 	public ReportCard viewReportCard(String studentId);
-	
+
+	/**
+	 *
+	 * @param studentId
+	 * @return total fees paid
+	 * @throws PaymentNotFoundException on unsuccessful payment
+	 */
 	public double payFee(String studentId) throws PaymentNotFoundException;
 	
 }
