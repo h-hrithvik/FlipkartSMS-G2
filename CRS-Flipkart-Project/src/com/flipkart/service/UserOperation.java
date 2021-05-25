aeceddd2b2078b851034f8e0f8947fe76f0fbeb6
namanmanish <namanmanishtrivedi@gmail.com>
2021-05-25 13:18:30 +0530

    Banch issues

--- CRS-Flipkart-Project/src/com/flipkart/service/UserOperation.java
+++ CRS-Flipkart-Project/src/com/flipkart/service/UserOperation.java
@@ -1,23 +1,25 @@
 package com.flipkart.service;
 
 import com.flipkart.bean.User;
+import com.flipkart.exception.UserAlreadyExistException;
+import com.flipkart.exception.UserNotFoundException;
 
 public class UserOperation implements UserInterface {
 
 	@Override
-	public boolean addUser(User user) {
+	public boolean addUser(User user) throws UserAlreadyExistException{
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean verifyCredentials(String userId, String userPassword) {
+	public boolean verifyCredentials(String userId, String userPassword) throws UserNotFoundException {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean changePassword(String userId, String newPassword) {
+	public boolean changePassword(String userId, String newPassword) throws UserNotFoundException {
 		// TODO Auto-generated method stub
 		return false;
 	}
